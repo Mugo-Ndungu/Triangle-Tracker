a39dcb495ff1af3f94287696d024b27f7669f70a
Mugo-Ndungu <twinnymugo@gmail.com>
2018-12-02 17:16:30 +0300

    Add function for the tracker

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+
+
+function results() {
+  var triangle = [];
+  var flength = parseInt(document.getElementById('firstside').value);
+  var slength = parseInt(document.getElementById('firstside').value);
+  var tlength = parseInt(document.getElementById('firstside').value);
+  var resul = document.querySelector("#para");
+
+
+  triangle.push(flength);
+  triangle.push(slength);
+  triangle.push(tlength);
+
+  if (flength === NaN || slength === NaN || tlength === NaN) {
+    resul.innerHTML="Alert the Input requires a Number";
+  } else if (flength == slength && slength == tlength) {
+    resul.innerHTML="Thats an Equilateral Triangle. Cheers!" ;
+  } else if (flength == slength || slength == tlength || tlength == flength) {
+    resul.innerHTML="Thats an Isosceles Triangle. Cheers!" ;
+  } else if (flength != slength && slength != tlength && tlength != flength) {
+    resul.innerHTML="Thats a Scalene Triangle. Cheers!" ;
+  } else if (flength + slength>tlength || slength + tlength>flength || tlength + flength > slength){
+    resul.innerHTML="Thats not a Triangle. Cheers!" ;
+  }
+}
+//
+// function test(){
+//   var flength = document.getElementById('firstside');
+//   alert(flength);
+//
+// }
