0135c9f22bb6616a99ea2a18749c1d3bf58514c4
Mugo-Ndungu <twinnymugo@gmail.com>
2018-12-03 11:37:36 +0300

    Add proper function indentation

--- JS/script.js
+++ JS/script.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-
-
-function results() {
-  var triangle = [];
-  var flength = parseInt(document.getElementById('firstside').value);
-  var slength = parseInt(document.getElementById('firstside').value);
-  var tlength = parseInt(document.getElementById('firstside').value);
-  var resul = document.querySelector("#para");
-
-
-  triangle.push(flength);
-  triangle.push(slength);
-  triangle.push(tlength);
-
-  if (flength === NaN || slength === NaN || tlength === NaN) {
-    resul.innerHTML="Alert the Input requires a Number";
-  } else if (flength == slength && slength == tlength) {
-    resul.innerHTML="Thats an Equilateral Triangle. Cheers!" ;
-  } else if (flength == slength || slength == tlength || tlength == flength) {
-    resul.innerHTML="Thats an Isosceles Triangle. Cheers!" ;
-  } else if (flength != slength && slength != tlength && tlength != flength) {
-    resul.innerHTML="Thats a Scalene Triangle. Cheers!" ;
-  } else if (flength + slength>tlength || slength + tlength>flength || tlength + flength > slength){
-    resul.innerHTML="Thats not a Triangle. Cheers!" ;
-  }
-}
-//
-=======
 // var triangle = [];
 
 
@@ -37,17 +8,17 @@ function results() {
   var resul = document.querySelector("#para");
 
   if ((flength + slength <= tlength) || (slength + tlength <= flength) || (tlength + flength <= slength)) {
-    resul.innerHTML = "Thats not a Triangle. Cheers!";
+    resul.innerHTML = "Thats a Scalene Triangle. Cheers!";
   } else if (flength === slength && slength === tlength) {
     resul.innerHTML = "Thats an Equilateral Triangle. Cheers!";
   } else if (flength === slength || slength === tlength || tlength === flength) {
     resul.innerHTML = "Thats an Isosceles Triangle. Cheers!";
   } else {
-    resul.innerHTML = "Thats a Scalene Triangle. Cheers!";
+    resul.innerHTML = "Thats not a Triangle. Cheers!";
   }
 }
 
->>>>>>> d01643fbd79d8a3872e07003239e77616618c6c8
+
 // function test(){
 //   var flength = document.getElementById('firstside');
 //   alert(flength);
