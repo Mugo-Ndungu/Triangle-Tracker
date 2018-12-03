0d29da82bd34cebbc9644aa572a0ac8f0fe2ee67
Mugo-Ndungu <twinnymugo@gmail.com>
2018-12-03 11:25:39 +0300

    Add a working function for Equilateral

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,22 @@
 // var triangle = [];
-var flength = parseInt(document.getElementById('firstside').value);
-var slength = parseInt(document.getElementById('firstside').value);
-var tlength = parseInt(document.getElementById('firstside').value);
-var resul = document.querySelector("#para");
+
 
 function results() {
-  // triangle.push(flength);
-  // triangle.push(slength);
-  // triangle.push(tlength);
-  if (isNaN(flength) && isNaN(slength) && isNaN(tlength)) {
-    if ((flength + slength) < tlength || (slength + tlength) < flength || (tlength + flength) < slength) {
-      resul.innerHTML = "Thats not a Triangle. Cheers!";
-    } else if (flength === slength && slength === tlength) {
-      resul.innerHTML = "Thats an Equilateral Triangle. Cheers!";
-    } else if (flength === slength || slength === tlength || tlength === flength) {
-      resul.innerHTML = "Thats an Isosceles Triangle. Cheers!";
-    } else if (flength != slength && slength != tlength && tlength != flength) {
-      resul.innerHTML = "Thats a Scalene Triangle. Cheers!";
-    }
+  var flength = parseFloat(document.getElementById('firstside').value);
+  var slength = parseFloat(document.getElementById('secondside').value);
+  var tlength = parseFloat(document.getElementById('thirdside').value);
+  var resul = document.querySelector("#para");
+
+  if ((flength + slength <= tlength) || (slength + tlength <= flength) || (tlength + flength <= slength)) {
+    resul.innerHTML = "Thats not a Triangle. Cheers!";
+  } else if (flength === slength && slength === tlength) {
+    resul.innerHTML = "Thats an Equilateral Triangle. Cheers!";
+  } else if (flength === slength || slength === tlength || tlength === flength) {
+    resul.innerHTML = "Thats an Isosceles Triangle. Cheers!";
+  } else {
+    resul.innerHTML = "Thats a Scalene Triangle. Cheers!";
   }
 }
-
-
 
 // function test(){
 //   var flength = document.getElementById('firstside');
